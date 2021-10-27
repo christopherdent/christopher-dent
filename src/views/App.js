2b3aed6e6040852a643b0af340a69473d6066aed
Christopher Dent <christopherdent01@gmail.com>
2021-10-26 22:08:15 -0400

    router edits

--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,10 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import { createBrowserHistory } from "history";
+
+import { BrowserRouter, Route, Switch, Redirect, Router } from "react-router-dom";
 
 // core components
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
@@ -12,38 +15,33 @@ import Blog from "containers/Blog.js";
 import DarkFooter from "components/Footers/DarkFooter.js";
 
 
- 
+var hist = createBrowserHistory();
 
 function App() {
-  // React.useEffect(() => {
-  //   document.body.classList.add("index-page");
-  //   document.body.classList.add("sidebar-collapse");
-  //   document.documentElement.classList.remove("nav-open");
-  //   window.scrollTo(0, 0);
-  //   document.body.scrollTop = 0;
-  //   return function cleanup() {
-  //     document.body.classList.remove("index-page");
-  //     document.body.classList.remove("sidebar-collapse");
-  //   };
-  // });
+  React.useEffect(() => {
+    document.body.classList.add("index-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("index-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  });
   return (
     <>
   <IndexNavbar />
    <div className="wrapper">     
-      <BrowserRouter>
-        <Switch>
-          <Switch>
-          <Route exact path='/' component={Main} />
-          <Route exact path='/projects' component={Projects} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/blog' component={Blog} />
-        
-
-
- 
-        </Switch>
-      </Switch>
-    </BrowserRouter>
+   <Router history={hist}>
+    <Switch>
+      <Route path="/christopher-dent" component={Main} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/about" component={About} />
+      <Route path="/" component={Main} />
+    </Switch>
+  </Router>,
    <DarkFooter />
   </div>
  </>
