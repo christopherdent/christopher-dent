94b298a9e861f4ed64b5b0ad9ab095478697bf2f
Christopher Dent <christopherdent01@gmail.com>
2021-10-26 22:36:00 -0400

    add hashrouter fix typo

--- src/views/App.js
+++ src/views/App.js
@@ -35,7 +35,7 @@ function App() {
   <IndexNavbar />
    <div className="wrapper">     
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={Main} />
       <Route path="/about" component={About} />
       <Route exact path="/projects" component={Projects} />
       <Route exact path="/blog" component={Blog} />
