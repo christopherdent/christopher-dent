33dc11f0c5b859c1da8ef4b31c08595328ddc507
Christopher Dent <christopherdent01@gmail.com>
2021-11-12 20:36:37 -0500

    add opacity effect to social icons and moved to footer

--- src/components/Social.js
+++ src/components/Social.js
@@ -13,14 +13,16 @@ import { Container } from "reactstrap";
 function Social() {
 return (
 <div id="social">
-    <SocialIcon url="https://github.com/christopherdent" />
+
+    <SocialIcon url="https://christopher-dent.medium.com/" bgColor="#E1AD01" />
+    
     <SocialIcon url="https://www.linkedin.com/in/christopher-james-dent/" />
+    
+    <SocialIcon url="https://github.com/christopherdent" bgColor='#7DF9FF'/>
+
+    <SocialIcon url="https://www.youtube.com/channel/UCZBxvf3wO37Ymx-mO8tPwLQ/videos" />   
 
     <SocialIcon url="https://www.facebook.com/christopher.j.dent" />
-
-    <SocialIcon url="https://www.youtube.com/channel/UCZBxvf3wO37Ymx-mO8tPwLQ/videos" />
-
-    <SocialIcon url="https://christopher-dent.medium.com/" />
 </div>
     
     )
