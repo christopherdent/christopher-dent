536a2e901159bd832ead127e0c37e1911e8a8f15
Christopher Dent <christopherdent01@gmail.com>
2021-11-18 23:03:39 -0500

    add email social icon, remove medium as redundant

--- src/components/Social.js
+++ src/components/Social.js
@@ -14,15 +14,14 @@ function Social() {
 return (
 <div id="social">
 
-    <SocialIcon url="https://christopher-dent.medium.com/" bgColor="#E1AD01" />
-    
+        
     <SocialIcon url="https://www.linkedin.com/in/christopher-james-dent/" />
     
     <SocialIcon url="https://github.com/christopherdent" bgColor='#7DF9FF'/>
 
     <SocialIcon url="https://www.youtube.com/channel/UCZBxvf3wO37Ymx-mO8tPwLQ/videos" />   
 
-   
+    <SocialIcon url="mailto: christopherdent01@gmail.com"  />
 
     <SocialIcon url="https://twitter.com/christopjdent" />
 
