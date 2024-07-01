109fdbf52bd6ac29225e19754e2ce00eec963bf1
= <christopher.dent@opscompass.com>
2024-07-01 11:54:32 -0400

    Updates about bg for readability

--- src/containers/About.js
+++ src/containers/About.js
@@ -3,10 +3,7 @@
 import React, { useState } from 'react';
 
 // reactstrap components
-import { Button, Container, Card, CardBody, CardSubtitle, Media, Collapse, Col, Row } from "reactstrap";
-
-
-
+import { Button, Container, Card, CardBody, Media, Collapse, Col, Row } from "reactstrap";
 
 function About() {
 
@@ -47,55 +44,51 @@ function About() {
         </div>
       
        <Container>
-        
-        
-       <h3 className="title">About Me</h3> 
+       <h3 className="title">About Me</h3>
        </Container>
 
 
       <Collapse isOpen={!isOpen}>
 
-      <Container className = "aboutContainer">
+      <Container className = "aboutContainer" >
   
        <Row>
          <Col className = "lg-4 d-flex align-items-stretch">
-      
       <Card id='leftAbout'>
        <CardBody>
          <h4 className="card-subtitle mb-2 text-muted">My Skills</h4>
-         <p className="card-text">Flatiron taught me Ruby, Sinatra, Ruby on Rails, and JavaScript (including React/Redux). Comfortable with SQL databases including PostgreSQL and MySQL. More recently taught myself Angular. Familiar with but still learning C++, Java and various other technologies. Continuous learning is what I love about code. I do this in my spare time and only wish I had more time to devote to it! I have strong organizational skills and international experience across a range of industries. Certified Project Manager (PMI, PRINCE2) and Change Manager (Prosci).</p>
+         <p className="card-text">Flatiron taught me Ruby, Sinatra, Ruby on Rails, and JavaScript (including React/Redux). My job has led to me becoming proficient in Typescript and VueJS as well.  Comfortable with SQL databases including PostgreSQL and MySQL. More recently taught myself Angular. Familiar with but still learning C++, Java and various other technologies. Continuous learning is what I love about code. I do this in my spare time and only wish I had more time to devote to it! I have strong organizational skills and international experience across a range of industries. Certified Project Manager (PMI, PRINCE2) and Change Manager (Prosci).</p>
         </CardBody>
       </Card>
+     </Col>
 
-          </Col>
-
-          <Col className = "lg-4 d-flex align-items-stretch">
+     <Col className = "lg-4 d-flex align-items-stretch">
       <Card id='middleAbout'>
         <CardBody>
         <h4 className="card-subtitle mb-2 text-muted">My Experience</h4>
-        <p className="card-text">I graduated from McGill University in Canada with a double major in political science and history. I've been working in various project management roles pretty much ever since - mostly at a DC-based nonprofit analytical standards development association, but with a brief stint in the UK where I worked contracts for the Royal Bank of Scotland Group and the City of Edinburgh Council. I've got approximately 12 years project management experiece, all the while learning code in my spare time.</p>  
-        </CardBody>
-      </Card>
-          </Col>
+        <p className="card-text">I graduated from McGill University in Canada with a double major in political science and history. I've been working in various project management roles pretty much ever since - mostly at a DC-based nonprofit analytical standards development association, but with a brief stint in the UK where I worked for the City of Edinburgh Council as an IT Project Manager. I've got approximately 12 years project management experiece, all the while learning code in my spare time.</p>  
+      </CardBody>
+    </Card>
+  </Col>
 
 
-<Col className = "lg-4 d-flex align-items-stretch">
-<Card id='rightAbout'>
-   <CardBody>
+  <Col className = "lg-4 d-flex align-items-stretch">
+    <Card id='rightAbout'>
+      <CardBody>
     
-    <h4 className="card-subtitle mb-2 text-muted">My Goals</h4>
-    <p className="card-text">Since I was young, I've always been fascinated by code and tech in general. I am looking for any and all opportunities to put my coding skills to good use, ideally leveraging my extensive project management and/or association management experience. I'm fascinated with the concept of creating amazing programs from nothing but a text editor and I'm hoping to make it more than just a hobby someday soon. Have a project you're like to talk about? Drop me line using one of the social contacts below.</p>
+          <h4 className="card-subtitle mb-2 text-muted">My Goals</h4>
+          <p className="card-text">Since I was young, I've always been fascinated by code and tech in general. 
+            I am looking for any and all opportunities to put my coding skills to good use, 
+            ideally leveraging my extensive project management and/or association management experience. 
+            I'm fascinated with the concept of creating amazing programs from nothing but a text editor and really glad to have made this career change.
+              Have a project you're like to talk about? Drop me line using one of the social contacts below.
+          </p>
    
-  </CardBody>
-</Card>
- 
-</Col>
-
-
-
-</Row>
-
-</Container>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
 </Collapse>
 
             <center>
@@ -116,7 +109,7 @@ function About() {
 
     <Collapse isOpen={isOpen}>
 
-      <Container>
+      <Container style={{ backgroundColor: 'navy' }}>
 
           <Media>
    
